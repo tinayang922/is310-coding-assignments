b0992402859fb04ffd5d5eb8d964e5e140983af0
Wei-Ting <wtyang2@illinois.edu>
2024-10-01 09:50:55 -0500

    Completed homework for Python Foundations

---
+++ python-scripting/first_script.py
@@ -0,0 +1,14 @@
+favorite_movies =[
+    {
+        "name": "The Matrix I",
+        "release_year": 1999,
+        "sequels": ["The Matrix II", "The Matrix III", "The Matrix IV"]
+    },
+    {
+        "name": "Star Wars IV",
+        "release_year": 1977,
+        "sequels": ["Star Wars V", "Star Wars VI", "Star Wars VII", "Star Wars VIII", "Star Wars IX"],
+        "prequels": ["Star Wars I", "Star Wars II", "Star Wars III"]
+    }
+]
+
